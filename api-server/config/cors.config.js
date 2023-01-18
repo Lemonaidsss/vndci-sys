100426a5a4ec19cb134ba818a8961da7075e531c
Adrian <ramirezadrianfrancis@gmail.com>
2023-01-19 00:11:36 +0800

    Update cors.config.js
    added front-end link

--- api-server/config/cors.config.js
+++ api-server/config/cors.config.js
@@ -3,6 +3,7 @@ const whitelist = [
   "https://yourdomain.com",
   "http://127.0.0.1:5000",
   "http://localhost:3000",
+  "https://vndci-sys.vercel.app/login"
 ];
 
 // !origin for dev enviroment only
