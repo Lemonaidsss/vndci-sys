6506f032c7e05ef5e1c1f260cba68b06378765ac
Adrian <ramirezadrianfrancis@gmail.com>
2023-01-19 00:41:44 +0800

    updated api-base-url

--- public-client/src/config/config.js
+++ public-client/src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_BASE_URL: "https://63c81cae0d107105d668c755--coruscating-faloodeh-4f0366.netlify.app/",
+  API_BASE_URL: "https://vndci-sys-api-server.onrender.com/",
   RESOURCES_BASE_URL: "http://localhost:9000",
 };
 
